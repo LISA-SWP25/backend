c3a3a4a7ccf0060bcb1b726f955795b21def7d1c
merikhimo <merikhimo@gmail.com>
2025-06-22 14:34:18 +0300

    тестим апишку

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://lisa:pass@localhost:5432/lisa_dev"
+DATABASE_URL = "postgresql://merikhimo@localhost:5432/lisa_dev"
 
 engine = create_engine(DATABASE_URL)
 
